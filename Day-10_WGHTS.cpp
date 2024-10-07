7cfa914c681a55cd34aee692300b6dbd6b5bc8e1
s0urabh0703 <154411269+s0urabh0703@users.noreply.github.com>
2024-10-07 19:00:46 +0530

    Add files via upload

---
+++ Day-10_WGHTS.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int w,a,b,c;
+	    cin >> w >> a>>b>>c;
+	    if(w==a or w==b or w==c)
+	    cout << "YES" << endl;
+	    else if(((a+b) == w) or ((b+c)==w) or((c+a)==w) or (w==(a+b+c)))
+	    cout << "YES" << endl;
+	    else
+	    cout << "NO" << endl;
+	}
+	return 0;
+}

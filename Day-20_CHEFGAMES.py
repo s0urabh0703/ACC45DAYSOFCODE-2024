a2214eae21362ac5d072315e6946fe27467fa28b
s0urabh0703 <154411269+s0urabh0703@users.noreply.github.com>
2024-10-22 18:21:01 +0530

    Add files via upload

---
+++ Day-20_CHEFGAMES.py
@@ -0,0 +1,8 @@
+# cook your dish here
+# cook your dish here
+for i in range(int(input())):
+    r1,r2,r3,r4=map(int,input().split())
+    if (r1+r2+r3+r4)==0:
+        print("IN")
+    else:
+        print("OUT")

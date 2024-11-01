56ad474d08cd63eddef47c41fa2cfb8e1b8739e1
s0urabh0703 <154411269+s0urabh0703@users.noreply.github.com>
2024-11-01 17:07:01 +0530

    Add files via upload

---
+++ Day-30_TRUESCORE.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--){
+	    int a,b,c,d;
+	    cin>>a>>b;
+	    cin>>c>>d;
+	    
+	    if(c < a || d < b) cout<<"IMPOSSIBLE"<<endl;
+	    else cout<<"POSSIBLE"<<endl;
+	    
+	    
+	}
+	return 0;
+}
